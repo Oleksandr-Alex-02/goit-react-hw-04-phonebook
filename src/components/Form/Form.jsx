9d4218959f7b344626d70e92e8c74addbbe302f1
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2023-02-28 13:36:41 +0200

    handlChangeValue

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -94,3 +94,82 @@ Form.propTypes = {
     contactsName: PropTypes.arrayOf(PropTypes.string.isRequired),
 };
 
+
+
+
+
+
+// import React, { useState } from 'react'
+// import PropTypes from 'prop-types'
+
+// import css from './Form.module.css'
+
+
+// export default function Form({ onSubmit }) {
+//     const [name, setName] = useState('');
+//     const [number, setNumber] = useState('');
+
+//     const handlChangeValue = evt => {
+//         const { name, value } = evt.target;
+
+//         switch (name) {
+//             case 'name':
+//                 setName(value)
+//                 break;
+
+//             case 'number':
+//                 setNumber(value)
+//                 break;
+
+//             default:
+//                 console.error('error');
+//         }
+//     }
+
+
+//     const handleSubmit = (evt) => {
+//         evt.preventDefault();
+
+//         // const { name, value } = evt.target;
+//         // this.setState({ [name]: value });
+//         console.log(name, number)
+//         setName('')
+//         setNumber('')
+//     };
+
+//     return (
+//         <>
+//             <div className={css.blok__form}>
+//                 <form className={css.form} onSubmit={handleSubmit}>
+//                     <label className={css.label}>
+//                         Name
+//                         <input
+//                             className={css.input}
+//                             onChange={handlChangeValue}
+//                             value={name}
+//                             type="text"
+//                             name="name"
+//                             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//                             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//                             required
+//                         />
+//                     </label>
+//                     <label>
+//                         Number
+//                         <input
+//                             className={css.input}
+//                             onChange={handlChangeValue}
+//                             value={number}
+//                             type="tel"
+//                             name="number"
+//                             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//                             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//                             required
+//                         />
+//                     </label>
+//                     <button type="submit">App contacts</button>
+//                 </form>
+//             </div>
+//         </>
+//     )
+// }
